e2f2e75c191d2759d88ce4d285ac5dc7179abe29
Nico Arato <nico.arato@gmail.com>
2019-06-11 04:03:49 -0300

    Primeros pasos

---
+++ aprendiendo-JavaScript/hola-mundo/js/01-hola-mundo.js
@@ -0,0 +1,7 @@
+
+alert('Hola mundo');
+document.write("Hola mundo desde fichero");
+console.log("Muestra mensaje en la consola");
+console.log(5+9);
+
+
---
+++ aprendiendo-JavaScript/hola-mundo/js/02-variables.js
@@ -0,0 +1,12 @@
+//variables
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+
+var pais_continente = pais +" "+ continente;
+
+pais = "Honduras";
+continente = "america";
+console.log(pais, continente,antiguedad);
+alert(pais_continente);
