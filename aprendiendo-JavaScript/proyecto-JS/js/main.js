5355d41fc352786d059456f56055cbaf532b9251
Nico Arato <nico.arato@gmail.com>
2019-08-19 01:52:18 -0300

    Acordeon

--- aprendiendo-JavaScript/proyecto-JS/js/main.js
+++ aprendiendo-JavaScript/proyecto-JS/js/main.js
@@ -1,51 +1,54 @@
 $(document).ready(function(){
 
     //SLIDER
-    $(function(){
-        $('.bxslider').bxSlider({
-          mode: 'fade',
-          captions: true,
-          slideWidth: 1200
+    if(window.location.href.indexOf('index')> -1){
+        $(function(){
+            $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200
+            });
         });
-      });
+    }
 
     //POST
-    var posts = [
-        {title: 'Prueba de titulo 1',
-          date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
-          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
-        },
-        {title: 'Prueba de titulo 2',
-          date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
-          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
-        },
-        {title: 'Prueba de titulo 3',
-          date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
-          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
-        },
-        {title: 'Prueba de titulo 4',
-          date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
-          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
-        },
-        {title: 'Prueba de titulo 5',
-          date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
-          content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
-        }
+    if(window.location.href.indexOf('index')> -1){
+        var posts = [
+            {title: 'Prueba de titulo 1',
+            date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
+            },
+            {title: 'Prueba de titulo 2',
+            date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
+            },
+            {title: 'Prueba de titulo 3',
+            date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
+            },
+            {title: 'Prueba de titulo 4',
+            date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
+            },
+            {title: 'Prueba de titulo 5',
+            date: 'Publicado el día:' + moment().date() +"-"+ moment().format("MM-YYYY"),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda asperiores modi sapiente amet nesciunt ea, numquam veniam ad! Provident asperiores odio eum nulla unde dignissimos consequatur architecto perferendis. Quam?"
+            }
 
-    ];
+        ];
 
-    posts.forEach((item, index) => {
+        posts.forEach((item, index) => {
 
-        var post = `<article class="post">
-                        <h2>${item.title}</h2>
-                        <span class="date">${item.date}</span>
-                        <p>${item.content}</p>
-                        <a href="#" class="button-more">LEER MAS</a>
-                    </article>`;
+            var post = `<article class="post">
+                            <h2>${item.title}</h2>
+                            <span class="date">${item.date}</span>
+                            <p>${item.content}</p>
+                            <a href="#" class="button-more">LEER MAS</a>
+                        </article>`;
 
-        $("#posts").append(post);
-    });
-
+            $("#posts").append(post);
+        });
+    }
     //selector de tema
     var theme = $("#theme");
     $("#to-green").click(function(){
@@ -91,5 +94,11 @@ $(document).ready(function(){
             location.reload();
         });
     }
+
+    if(window.location.href.indexOf('about')> -1){
+        $("#acordeon").accordion();
+
+    }
+
 
 });
