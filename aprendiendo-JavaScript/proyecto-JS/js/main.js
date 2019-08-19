0f0cf72a1e06b09bdc0f7e8eb8d07b8e9f1036ef
Nico Arato <nico.arato@gmail.com>
2019-08-19 01:17:33 -0300

    Login falso con localStorage.

--- aprendiendo-JavaScript/proyecto-JS/js/main.js
+++ aprendiendo-JavaScript/proyecto-JS/js/main.js
@@ -72,6 +72,24 @@ $(document).ready(function(){
         return false;
     });
 
+    //login falso
 
+    $("#login form").submit(function(){
+        var form_name = $("#name").val();
+        localStorage.setItem("form_name", form_name);
+    });
+
+    var form_name = localStorage.getItem("form_name");
+
+    if(form_name != null && form_name != 'undefined'){
+        var about_parrafo = $("#about p");
+        about_parrafo.html("<br><strong>Bienvenido, "+ form_name + "</strong>" );
+        about_parrafo.append("<a href='#' id='logout'> Cerrar sesión </a>");
+        $("#login").hide();
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+    }
 
 });
