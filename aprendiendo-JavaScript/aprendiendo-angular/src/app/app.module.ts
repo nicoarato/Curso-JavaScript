e1469366e593a7b7ac60ed368b4af7152e185ea1
Nico Arato <nico.arato@gmail.com>
2019-09-16 01:51:50 -0300

    Data binding.
    Two way data-binding bidereccional.

--- aprendiendo-JavaScript/aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-JavaScript/aprendiendo-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { VideojuegoComponent} from './videojuego/videojuego.component';
@@ -14,7 +15,8 @@ import { CursosComponent } from './cursos/cursos.component';
     CursosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- aprendiendo-JavaScript/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-JavaScript/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -11,6 +11,7 @@ export class ZapatillasComponent implements OnInit{
     public zapatillas: Array<Zapatilla>;
     public marcas: String[];
     public color:string;
+    public mi_marca:string;
 
     constructor(){
         this.color = 'yellow';
@@ -37,6 +38,13 @@ export class ZapatillasComponent implements OnInit{
             
         });
         console.log(this.marcas);
+    }
+
+    getMarca(){
+        alert(this.mi_marca);
+    }
+    addMarca(){
+        this.marcas.push(this.mi_marca);
     }
 
 }
