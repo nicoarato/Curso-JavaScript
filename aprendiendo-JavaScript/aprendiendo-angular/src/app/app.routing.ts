2cc6e9effd50e9aa91d8095bafc99c88c750b3ee
Nico Arato <nico.arato@gmail.com>
2019-09-16 12:13:36 -0300

    Un menú de navegación.
    routerLink: nos llevará al componente especificado.
    
    [routerLinkActive]="['activado']" cuando esté activado el componente se puede agregar estilo css a la clase 'activado'.

---
+++ aprendiendo-JavaScript/aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,28 @@
+//Aqui daremos de alta las nuevas rutas
+
+//importar modulos del router de Angular
+
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//importar componentes
+
+import { ZapatillasComponent} from './zapatillas/zapatillas.component';
+import { VideojuegoComponent} from './videojuego/videojuego.component';
+import {CursosComponent} from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+//import { ModuleWithProviders } from '@angular/compiler/src/core';
+
+//Array de rutas
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'zapatillas', component: ZapatillasComponent},
+    {path: 'videojuego', component: VideojuegoComponent},
+    {path: 'cursos', component: CursosComponent},
+    {path: '**', component: HomeComponent}
+];
+
+//Exportar el modulo del router
+export const appRoutingProviders: any[]= []; //servicio
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes); //modulo
