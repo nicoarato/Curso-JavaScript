35e328e55aafc14e60702faf1e4e8b4d49cfa525
Nico Arato <nico.arato@gmail.com>
2019-09-17 21:34:51 -0300

    Routing y redirecciones.

--- aprendiendo-JavaScript/aprendiendo-angular/src/app/cursos/cursos.component.ts
+++ aprendiendo-JavaScript/aprendiendo-angular/src/app/cursos/cursos.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+//para pasar datos por url necesitas algunas libreerias
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+
 @Component({
   selector: 'cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  public nombre: string;
+  public followers: number;
 
-  constructor() { }
+  constructor( 
+      private _route: ActivatedRoute,
+      private _router: Router
+      ) 
+      {
+  }
 
   ngOnInit() {
+    this._route.params.subscribe((params: Params) => {
+        this.nombre= params.nombre;
+        //this.followers= parseInt(params.followers);
+        this.followers= +params.followers;
+
+        //this.nombre= params['nombre'];
+
+        console.log(this.nombre + ' '+this.followers +' followers');
+
+        if(this.nombre == 'ninguno'){
+          this._router.navigate(['/home']);        
+        }
+    });
+  }
+
+  redirigir(){
+    this._router.navigate(['/zapatillas']);
   }
 
 }
